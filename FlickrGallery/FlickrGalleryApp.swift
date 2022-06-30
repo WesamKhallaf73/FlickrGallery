cadf48ac3d86c7aad5d7f53ae3660c3f3c7a5940
WesamKhallaf <wesamkhallaf@gmail.com>
2022-06-30 12:45:55 +0200

    Initial Commit

---
+++ FlickrGallery/FlickrGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrGalleryApp.swift
+//  FlickrGallery
+//
+//  Created by Wesam Khallaf on 30/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlickrGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
